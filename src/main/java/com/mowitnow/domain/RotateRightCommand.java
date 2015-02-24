78ac97705dbf7ab92b71526ec200378a4b3bb85c
Martin Jeannot <martin.jeannot@outlook.com>
2015-02-24 23:49:36 +0100

    [Test] Add rotate right command

---
+++ src/main/java/com/mowitnow/domain/RotateRightCommand.java
@@ -0,0 +1,16 @@
+package com.mowitnow.domain;
+
+/**
+ * Created by phnx on 2/24/15.
+ */
+public class RotateRightCommand implements Command {
+
+    public RotateRightCommand(Mower mower) {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}

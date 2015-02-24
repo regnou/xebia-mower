3b2b7a819210af44c5a37c391c18650307f166d2
Martin Jeannot <martin.jeannot@outlook.com>
2015-02-24 23:42:19 +0100

    [Test] Add rotate left command

---
+++ src/test/java/com/mowitnow/domain/RotateLeftCommandTests.java
@@ -0,0 +1,22 @@
+package com.mowitnow.domain;
+
+import org.mockito.Mockito;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.mock;
+
+/**
+ * {@link com.mowitnow.domain.RotateLeftCommand} unit testing.
+ */
+@Test
+public class RotateLeftCommandTests {
+
+    public void Given_RotateLeftCommand_When_Execute_Then_VerifyThatMowerRotatedLeft() {
+        Mower lawnMower = mock(LawnMower.class);
+        RotateLeftCommand rotateLeftCommand = new RotateLeftCommand(lawnMower);
+
+        rotateLeftCommand.execute();
+
+        Mockito.verify(lawnMower).rotateLeft();
+    }
+}
